566b83cff1c6da61a816adefa81d24363eb80ac1
Oleg Ivinsky <gskitlz@gmail.com>
2021-08-01 12:05:12 +0300

    Add AppButton tests

---
+++ tests/unit/AppButton.spec.ts
@@ -0,0 +1,33 @@
+import { render, screen, fireEvent } from '@testing-library/vue'
+import '@testing-library/jest-dom'
+import AppButton from '@/components/AppButton.vue'
+
+describe('AppButton.vue', () => {
+  const btnText = 'Click here'
+
+  it('isVisible prop works correctly', async () => {
+    const { rerender } = render(AppButton, {
+      props: {
+        text: btnText,
+      },
+    })
+    expect(screen.getByText(btnText)).toBeVisible()
+
+    await rerender({ isVisible: true })
+    expect(screen.getByText(btnText)).toBeVisible()
+
+    await rerender({ isVisible: false })
+    expect(screen.getByText(btnText)).not.toBeVisible()
+  })
+
+  it('emits onClick event', async () => {
+    const { emitted } = render(AppButton, {
+      props: {
+        text: btnText,
+      },
+    })
+    const btn = screen.getByText(btnText)
+    await fireEvent.click(btn)
+    expect(emitted().onClick).toBeDefined()
+  })  
+})
