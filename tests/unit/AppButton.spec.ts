523e59e185dec43ee6245a74d232f28c7e5e0883
Oleg Ivinsky <gskitlz@gmail.com>
2021-08-01 12:25:14 +0300

    Add extra semantic test for AppButton

--- tests/unit/AppButton.spec.ts
+++ tests/unit/AppButton.spec.ts
@@ -5,6 +5,15 @@ import AppButton from '@/components/AppButton.vue'
 describe('AppButton.vue', () => {
   const btnText = 'Click here'
 
+  it('renders and is visible', () => {
+    render(AppButton, {
+      props: {
+        text: btnText,
+      },
+    })
+    expect(screen.getByText(btnText)).toBeVisible()
+  });
+
   it('isVisible prop works correctly', async () => {
     const { rerender } = render(AppButton, {
       props: {
